53c56c21e1c61d2d6750e57a9566f23ecbc54698
hoseok.ryu <dkflfkdeh@naver.com>
2024-04-01 22:01:22 +0900

    ~ Step 5

--- lib/app/namer/namer.dart
+++ lib/app/namer/namer.dart
@@ -8,21 +8,25 @@ class NamerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => NamerAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: const NamerPage(),
-      )
-    );
+        create: (context) => NamerAppState(),
+        child: MaterialApp(
+          title: 'Namer App',
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          ),
+          home: const NamerPage(),
+        ));
   }
 }
 
 class NamerAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class NamerPage extends StatelessWidget {
@@ -31,12 +35,51 @@ class NamerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<NamerAppState>();
+    var pair = appState.current;
+
     return Scaffold(
-        body: Column(
-      children: [
-        const Text('A random idea:'),
-        Text(appState.current.asLowerCase),
-      ],
-    ));
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: const Text('Next'))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displayMedium!
+        .copyWith(color: theme.colorScheme.onPrimary);
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: '${pair.first} ${pair.second}',
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:test_drive/app/demo/demo.dart';
 import 'package:test_drive/app/namer/namer.dart';
 
 void main() {
-  runApp(const NamerApp());
   runApp(const DemoApp());
+  runApp(const NamerApp());
 }
-
